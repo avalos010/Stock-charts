38b829d479f7f89bcb60d501e49fd277e7ef6c2c
JLuis Avalos <luizavalos40@gmail.com>
2023-01-16 16:08:32 -0600

    use highcharts

--- src/pages/search/Chart.tsx
+++ src/pages/search/Chart.tsx
@@ -1,20 +1,46 @@
-import { ResponsiveContainer } from "recharts";
 import { useChartData } from "../../hooks/useChartData";
 import { useParams } from "react-router-dom";
-import { Line } from "../../components/charts/Line";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import { useRef } from "react";
 
 export function Chart() {
   const { symbol } = useParams();
   const { data } = useChartData(symbol as string);
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+  const sortedDates = data?.response.sort(
+    // @ts-ignore
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+
+  const options: Highcharts.Options = {
+    title: {
+      text: symbol?.toUpperCase(),
+    },
+    yAxis: {
+      title: {
+        text: "Price ($)",
+      },
+    },
+    xAxis: {
+      categories: sortedDates?.map((i) => i.date),
+    },
+    //TODO: Figure out these ts errors.
+    series: [
+      //@ts-ignore
+      { name: "Open", data: sortedDates?.map((d) => d.open) },
+      //@ts-ignore
+      { name: "Close", data: sortedDates?.map((d) => d.close) },
+    ],
+  };
 
   if (data?.response) {
     return (
-      <div>
-        <h2>{symbol?.toUpperCase()}</h2>
-        <ResponsiveContainer height={400} width="90%">
-          <Line data={data} />
-        </ResponsiveContainer>
-      </div>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+        ref={chartComponentRef}
+      />
     );
   } else {
     return null;
