76f2f0abe5b8c211b6ef578c73db671939ac7c23
JLuis Avalos <luizavalos40@gmail.com>
2023-01-20 15:39:06 -0600

    simple calendar change

--- src/components/DateChange.tsx
+++ src/components/DateChange.tsx
@@ -5,6 +5,7 @@ import "react-datepicker/dist/react-datepicker.css";
 export function DateChange({ onChange, date }: DateChangeProps) {
   return (
     <DatePicker
+      showYearDropdown={true}
       selected={date}
       onChange={(date) => onChange(date as Date | string)}
     />
